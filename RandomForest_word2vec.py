8ce87cc82510209b5e58982f93030d5ad6b49f34
yangze01 <858848101@qq.com>
2017-04-20 09:23:41 +0800

    test

--- RandomForest_word2vec.py
+++ RandomForest_word2vec.py
@@ -17,6 +17,7 @@ from Segment import MySegment
 import gensim
 import datetime
 import matplotlib.pyplot as plt
+np.seterr(divide='ignore', invalid='ignore')
 from sklearn.metrics import confusion_matrix
 reload(sys)
 sys.setdefaultencoding('utf8')
