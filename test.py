bc6a1933d0ad8cb1edd7536331495c3237f52431
yangze01 <858848101@qq.com>
2017-04-24 20:18:35 +0800

    testest

--- test.py
+++ test.py
@@ -11,15 +11,17 @@ a_min = np.min(a, axis=0)
 a_max = np.max(a, axis=0)
 print(np.min(a, axis=0))
 print(np.max(a, axis=0))
+
 # print(a_min+a_max)
 # print(np.concatenate((a_min, a_max)))
+
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print(np.hstack((a_min,a_max)))
 print("++++++++++++++++++++++++++++++++++++++++++++")
 print(np.vstack((a_min,a_max)))
 b = a[1]
+
 # tmp_num = np.zeros(4)
-#
 # for i in range(0,1000):
 #     tmp_num = np.vstack((tmp_num,b))
 #     print(tmp_num.shape)
@@ -33,8 +35,6 @@ for key,value in a.items():
 
 # print np.extend()
 # np.extend(a_min, a_max)
-#
-#
 # def rf_similarity(path_vec):
 #     fea_size = len(path_vec)
 #     sim_vec = np.zeros([fea_size, fea_size])
@@ -46,11 +46,15 @@ for key,value in a.items():
 #             num_path = np.sum(path_vec[i] & path_vec[j])
 #             sim_vec[i][j] = num_path
 #             sim_vec[j][i] = num_path
-#     return sim_vec/sim_vec.diagonal().T#
+#     return sim_vec/sim_vec.diagonal().T
+
 import tensorflow as tf
+print(1)
 a = tf.constant(1)
 b = tf.constant(2)
 sess = tf.Session()
-print(sess.run(a+b))
+print(12345)
+
+# print(sess.run(a + b))
 # print(2123)
 
